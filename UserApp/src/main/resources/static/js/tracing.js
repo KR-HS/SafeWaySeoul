3c4f75776c3c6e9fbd34e248fde70ab07de7d1d9
JB170717 <bum2473@naver.com>
2025-04-16 20:19:34 +0900

    bus animation

--- UserApp/src/main/resources/static/js/tracing.js
+++ UserApp/src/main/resources/static/js/tracing.js
@@ -8,7 +8,29 @@ $(document).ready(function(){
 
 
     $(".mylocation").on('click',function(){
-        alert('현재위치버튼작동됨');
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var lat = position.coords.latitude;
+                var lng = position.coords.longitude;
+                var locPosition = new kakao.maps.LatLng(lat, lng);
+
+                console.log(lat);
+                console.log(lng);
+
+                // 지도 중심 이동
+                map.setCenter(locPosition);
+
+                // 마커 표시(선택)
+                new kakao.maps.Marker({
+                    map: map,
+                    position: locPosition
+                });
+            }, function(error) {
+                alert('위치 정보를 가져올 수 없습니다.');
+            }, );
+        } else {
+            alert('이 브라우저에서는 Geolocation을 지원하지 않습니다.');
+        }
     })
 
     $(".phonecall").on('click',function(){
