cd45da30835544c5761e9b43da1ec54ebb3ad89a
cypark728 <cypark448@gmail.com>
2025-04-16 22:45:31 +0900

    2025-04-16 childDetail Modal modify

--- DriverApp/src/main/resources/static/js/driver/manage.js
+++ DriverApp/src/main/resources/static/js/driver/manage.js
@@ -24,4 +24,10 @@ $(document).ready(function(){
     $(".close").on('click',function(){
         $(".modal").css("display","none");
     })
+
+    $(".modal").on('click', function () {
+        if (event.target.className.includes('modal')) {
+            $(this).css("display", "none");
+        }
+    });
 });
