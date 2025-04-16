e4d0bf46461c812bbea37d3715643032b8213441
KR-HS <jjc5989@naver.com>
2025-04-16 10:07:48 +0900

    수정

--- DriverApp/src/main/resources/static/js/modal/startManage.js
+++ DriverApp/src/main/resources/static/js/modal/startManage.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $(".cancel").on("click", function() {
         window.history.back();
     })
+    $(".start").on("click",function(){
+        window.location.href="/manage";
+    })
 });
