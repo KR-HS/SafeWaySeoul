37c5d3970e97c92ff0a8f051fe491e7c397a7d30
AhnJongsu <domain6591@gmail.com>
2025-04-24 19:17:34 +0900

    2025-04-24 7:16 DriverApp 주소 검사(?) 불러오기(?)

--- DriverApp/src/main/java/com/project/driverapp/kinder/mapper/KinderMapper.java
+++ DriverApp/src/main/java/com/project/driverapp/kinder/mapper/KinderMapper.java
@@ -1,7 +1,21 @@
 package com.project.driverapp.kinder.mapper;
 
+import com.project.driverapp.command.KinderVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface KinderMapper {
+
+    //주소 유효성 검사////////////////////
+    //2. KinderVo -> 매퍼에서 주소로 DB 조회
+    int checkAddress(@Param("kinderAddress") String kinderAddress,
+                     @Param("kinderPostcode") String kinderPostcode,
+                     @Param("kinderName") String kinderName);
+    /// ///////////////////////////////
+
+    List<KinderVO> getKinderList(String name);
+
 }
--- DriverApp/src/main/java/com/project/driverapp/kinder/service/KinderService.java
+++ DriverApp/src/main/java/com/project/driverapp/kinder/service/KinderService.java
@@ -1,4 +1,14 @@
 package com.project.driverapp.kinder.service;
 
+import com.project.driverapp.command.KinderVO;
+
+import java.util.List;
+
 public interface KinderService {
+
+    //주소 유효성 검사////////
+    boolean isValidAddress(String kinderAddress, String kinderPostcode, String kinderName);
+    /// ///////////////////
+
+    public List<KinderVO> getKinderList(String name);
 }
