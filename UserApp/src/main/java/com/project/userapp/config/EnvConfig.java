d2e1b9313a42b2d061a9cd306be3cc9f6ec3760e
KR-HS <jjc5989@naver.com>
2025-04-30 09:26:43 +0900

    aws env파일 추가

---
+++ UserApp/src/main/java/com/project/userapp/config/EnvConfig.java
@@ -0,0 +1,26 @@
+package com.project.userapp;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.*;
+
+public class EnvConfig implements EnvironmentPostProcessor {
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        Map<String, Object> dotenvProperties = new HashMap<>();
+        dotenvProperties.put("aws.accessKey", dotenv.get("AWS_ACCESS_KEY_ID"));
+        dotenvProperties.put("aws.secretKey", dotenv.get("AWS_SECRET_ACCESS_KEY"));
+        dotenvProperties.put("aws.region", dotenv.get("AWS_REGION"));
+        dotenvProperties.put("aws.bucket", dotenv.get("AWS_S3_BUCKET_NAME"));
+
+        environment.getPropertySources().addFirst(new MapPropertySource("dotenv", dotenvProperties));
+    }
+}
